1f5c695f7564c52cf60a49e47bb38d29a28bec8f
Luis Olivar <lao294@nyu.edu>
2019-02-16 13:58:12 -0500

    Added NYUClassData helper

---
+++ src/helperMethods/NYUClassData.js
@@ -0,0 +1,9 @@
+class NYUClassData {
+  constructor(data) {
+    this.nyu_course_id = data.nyu_course_id;
+    this.subject_code = data.subject_code;
+    this.course_title = data.course_title;
+  }
+}
+
+export default NYUClassData;
