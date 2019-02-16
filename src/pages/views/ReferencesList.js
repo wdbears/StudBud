5bd94590a006ae3ce0c6bd5ca50c6817f3493099
Ahmad Raza <raza2157@gmail.com>
2019-02-16 15:20:32 -0500

    Split Sidebar and MainView into seperate files

---
+++ src/pages/views/ReferencesList.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import { Layout, Icon, List } from "antd";
+
+const { Content } = Layout;
+const listData = [];
+for (let i = 0; i < 1; i++) {
+  listData.push({
+    href: "http://ant.design",
+    title: `Intro to Modern Design Principles`,
+    description:
+      "Ant Design, a design language for background applications, is refined by Ant UED Team.",
+    content:
+      "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently."
+  });
+}
+
+const IconText = ({ type, text }) => (
+  <span>
+    <Icon type={type} style={{ marginRight: 8 }} />
+    {text}
+  </span>
+);
+
+class Home extends Component {
+  render() {
+    return (
+      <Layout style={{ padding: "24px 24px 0" }}>
+        <Content
+          style={{
+            background: "#fff",
+            padding: 24,
+            margin: 0
+          }}
+        >
+          <div>
+            <span style={{ fontSize: 45 }}>Resources</span>
+          </div>
+          <div>
+            <List
+              itemLayout="vertical"
+              size="large"
+              pagination={{
+                onChange: page => {
+                  console.log(page);
+                },
+                pageSize: 3
+              }}
+              dataSource={listData}
+              renderItem={item => (
+                <List.Item
+                  key={item.title}
+                  actions={[
+                    <IconText type="star-o" text="156" />,
+                    <IconText type="like-o" text="156" />,
+                    <IconText type="message" text="2" />
+                  ]}
+                >
+                  <List.Item.Meta
+                    title={<a href={item.href}>{item.title}</a>}
+                    description={item.description}
+                  />
+                  {item.content}
+                </List.Item>
+              )}
+            />
+          </div>
+        </Content>
+      </Layout>
+    );
+  }
+}
+
+export default Home;
