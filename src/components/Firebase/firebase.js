357a85633f1410079b2c5007fc22ef76ffd46e29
Luis Olivar <lao294@nyu.edu>
2019-02-16 01:44:28 -0500

    Uncommented required code.

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,18 +1,18 @@
-// import app from 'firebase/app'
+import app from 'firebase/app'
 
-// const config = {
-//     apiKey: "AIzaSyCGueOD7Hwq9XmKeyTxZbykOSFOQb-Pjvo",
-//     authDomain: "stud-bud.firebaseapp.com",
-//     databaseURL: "https://stud-bud.firebaseio.com",
-//     projectId: "stud-bud",
-//     storageBucket: "stud-bud.appspot.com",
-//     messagingSenderId: "880365564000"
-//   };
+const config = {
+    apiKey: "AIzaSyCGueOD7Hwq9XmKeyTxZbykOSFOQb-Pjvo",
+    authDomain: "stud-bud.firebaseapp.com",
+    databaseURL: "https://stud-bud.firebaseio.com",
+    projectId: "stud-bud",
+    storageBucket: "stud-bud.appspot.com",
+    messagingSenderId: "880365564000"
+  };
 
-//   class Firebase{
-//       constructor(){
-//           app.initializeApp(config);
-//       }
-//   }
+  class Firebase{
+      constructor(){
+          app.initializeApp(config);
+      }
+  }
 
-//   export default Firebase;
+  export default Firebase;
