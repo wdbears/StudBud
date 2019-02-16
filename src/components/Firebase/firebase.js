2e356eebcec1321e4feb85f6bb51f51a0306bb3f
Luis Olivar <luisolivar@10-18-97-20.dynapool.wireless.nyu.edu>
2019-02-16 01:37:18 -0500

    Firebase Setup

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,18 @@
+import app from 'firebase/app'
+
+const config = {
+    apiKey: "AIzaSyCGueOD7Hwq9XmKeyTxZbykOSFOQb-Pjvo",
+    authDomain: "stud-bud.firebaseapp.com",
+    databaseURL: "https://stud-bud.firebaseio.com",
+    projectId: "stud-bud",
+    storageBucket: "stud-bud.appspot.com",
+    messagingSenderId: "880365564000"
+  };
+
+  class Firebase{
+      constructor(){
+          app.initializeApp(config);
+      }
+  }
+
+  export default Firebase;
