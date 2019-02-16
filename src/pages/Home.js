4b3e1fd919f7db506082c269887f5bd08807ce66
Jarman Liu <jarman124@gmail.com>
2019-02-16 16:21:42 -0500

    Changed sidebar
    It will close the other submenu when you click another submenu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,25 @@ const { Sider } = Layout;
 const SubMenu = Menu.SubMenu;
 
 class Home extends Component {
+  // submenu keys of first level
+  rootSubmenuKeys = ["sub1", "sub2", "sub4"];
+
+  state = {
+    openKeys: ["sub1"]
+  };
+
+  onOpenChange = openKeys => {
+    const latestOpenKey = openKeys.find(
+      key => this.state.openKeys.indexOf(key) === -1
+    );
+    if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+      this.setState({ openKeys });
+    } else {
+      this.setState({
+        openKeys: latestOpenKey ? [latestOpenKey] : []
+      });
+    }
+  };
   render() {
     return (
       <Layout style={{ minHeight: "92.3vh", zIndex: "-1" }}>
@@ -16,6 +35,8 @@ class Home extends Component {
               mode="inline"
               defaultSelectedKeys={["1"]}
               defaultOpenKeys={["sub1"]}
+              openKeys={this.state.openKeys}
+              onOpenChange={this.onOpenChange}
               style={{ borderRight: 0 }}
             >
               <SubMenu
