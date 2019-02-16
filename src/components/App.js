4ed8d090de983eea3e78c74f16188c3d57818cb4
Ahmad Raza <raza2157@gmail.com>
2019-02-16 16:44:30 -0500

    Hotfix

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Navbar from "./Navbar";
-import Home from "../pages/Home";
+import Sidebar from "../pages/Sidebar";
 import Login from "../pages/auth/Login";
 import Register from "../pages/auth/Register";
 import ClassesList from "../pages/views/ClassesList";
+import { Layout } from "antd";
 
 class App extends Component {
   render() {
@@ -12,10 +13,14 @@ class App extends Component {
       <BrowserRouter>
         <div>
           <Navbar />
-          <Route exact path="/" component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/classes" component={ClassesList} />
+          <Layout style={{ minHeight: "90vh", zIndex: "-1" }}>
+            <Sidebar />
+            <Layout>
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+              <Route path="/classes" component={ClassesList} />
+            </Layout>
+          </Layout>
         </div>
       </BrowserRouter>
     );
---
+++ src/pages/Sidebar.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import { Layout, Menu, Icon } from "antd";
+import { Link } from "react-router-dom";
+// import ResourceList from "./views/ResourceList";
+
+const { Sider } = Layout;
+const SubMenu = Menu.SubMenu;
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <Sider width={200} style={{ background: "#fff" }}>
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          defaultOpenKeys={["sub1"]}
+          style={{ borderRight: 0 }}
+        >
+          <SubMenu
+            key="sub1"
+            title={
+              <span>
+                <Icon type="mail" />
+                <span>Course One</span>
+              </span>
+            }
+          >
+            <Menu.Item key="1">Resources</Menu.Item>
+            <Menu.Item key="2">Discussion</Menu.Item>
+            <Menu.Item key="3">Mentors</Menu.Item>
+          </SubMenu>
+          <SubMenu
+            key="sub2"
+            title={
+              <span>
+                <Icon type="appstore" />
+                <span>Course Two</span>
+              </span>
+            }
+          >
+            <Menu.Item key="4">Resources</Menu.Item>
+            <Menu.Item key="5">Discussion</Menu.Item>
+            <Menu.Item key="6">Mentors</Menu.Item>
+          </SubMenu>
+          <SubMenu
+            key="sub4"
+            title={
+              <span>
+                <Icon type="setting" />
+                <span>Course Three</span>
+              </span>
+            }
+          >
+            <Menu.Item key="7">Resouces</Menu.Item>
+            <Menu.Item key="8">Discussion</Menu.Item>
+            <Menu.Item key="9">Mentors</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="0" style={{ position: "absolute", bottom: 5 }}>
+            <Icon type="plus" />
+            <span style={{ fontSize: 20 }}>
+              <Link to="/classes">Add Class</Link>
+            </span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+    );
+  }
+}
+
+export default Sidebar;
