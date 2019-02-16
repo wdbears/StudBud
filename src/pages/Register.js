d26de9922e1219df3aea8937afc882802cd3eb24
Jarman Liu <jarman124@gmail.com>
2019-02-16 10:17:18 -0500

    Created Register page

---
+++ src/pages/Register.js
@@ -0,0 +1,127 @@
+import React from "react";
+import {
+  Form,
+  Input,
+  Row,
+  Col,
+  Select,
+  Checkbox,
+  Button,
+  AutoComplete
+} from "antd";
+
+const { Option } = Select;
+
+class RegistrationForm extends React.Component {
+  state = {
+    confirmDirty: false,
+    autoCompleteResult: []
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+      }
+    });
+  };
+
+  handleConfirmBlur = e => {
+    const value = e.target.value;
+    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  };
+
+  compareToFirstPassword = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && value !== form.getFieldValue("password")) {
+      callback("Two passwords that you enter is inconsistent!");
+    } else {
+      callback();
+    }
+  };
+
+  validateToNextPassword = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && this.state.confirmDirty) {
+      form.validateFields(["confirm"], { force: true });
+    }
+    callback();
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+
+    return (
+      <Row
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "10vw"
+        }}
+      >
+        <Col span={6}>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Item label="E-mail">
+              {getFieldDecorator("email", {
+                rules: [
+                  {
+                    type: "email",
+                    message: "The input is not valid E-mail!"
+                  },
+                  {
+                    required: true,
+                    message: "Please input your E-mail!"
+                  }
+                ]
+              })(<Input />)}
+            </Form.Item>
+            <Form.Item label="Password">
+              {getFieldDecorator("password", {
+                rules: [
+                  {
+                    required: true,
+                    message: "Please input your password!"
+                  },
+                  {
+                    validator: this.validateToNextPassword
+                  }
+                ]
+              })(<Input type="password" />)}
+            </Form.Item>
+            <Form.Item label="Confirm Password">
+              {getFieldDecorator("confirm", {
+                rules: [
+                  {
+                    required: true,
+                    message: "Please confirm your password!"
+                  },
+                  {
+                    validator: this.compareToFirstPassword
+                  }
+                ]
+              })(<Input type="password" onBlur={this.handleConfirmBlur} />)}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator("agreement", {
+                valuePropName: "checked"
+              })(
+                <Checkbox>
+                  I have read the <a href="">agreement</a>
+                </Checkbox>
+              )}
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Register
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default Form.create({ name: "register" })(RegistrationForm);
