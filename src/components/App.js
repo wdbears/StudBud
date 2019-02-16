4236983381406742f2e6002187fdff65d75f6067
Ahmad Raza <raza2157@gmail.com>
2019-02-16 01:46:55 -0500

    Added login form

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Navbar from "./Navbar";
 import "./styles/App.css";
+import Login from "../pages/Login";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <Login />
       </div>
     );
   }
