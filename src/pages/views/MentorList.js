e13678a86703ef28c1585a3557c3a097a0d84860
Jarman Liu <jarman124@gmail.com>
2019-02-17 10:58:52 -0500

    Added mentor pictures

--- src/pages/views/MentorList.js
+++ src/pages/views/MentorList.js
@@ -17,31 +17,45 @@ const init = {
 const data = [
   {
     title: "Abdullah Gulfam",
-    description: "Calculus"
+    description: "Calculus",
+    picture:
+      "https://scontent-lga3-1.xx.fbcdn.net/v/t1.15752-9/s2048x2048/51925003_574089206392086_135396838256672768_n.jpg?_nc_cat=103&_nc_ht=scontent-lga3-1.xx&oh=b8837bda3879adf23e4b903b1d743656&oe=5CE64BB7"
   },
   {
     title: "Brandon Yee",
-    description: "Data Structure"
+    description: "Data Structure",
+    picture:
+      "https://scontent-lga3-1.xx.fbcdn.net/v/t1.15752-9/52002814_554695864940523_1034874397195763712_n.jpg?_nc_cat=110&_nc_ht=scontent-lga3-1.xx&oh=7f2192e220de4a4055574489675b0aa8&oe=5CF1BE56"
   },
   {
     title: "Steve Wong",
-    description: "Head of Computer Science"
+    description: "Head of Computer Science",
+    picture:
+      "https://scontent-lga3-1.xx.fbcdn.net/v/t1.15752-9/48362267_347455619142954_7085968174587314176_n.jpg?_nc_cat=107&_nc_ht=scontent-lga3-1.xx&oh=877ae34a02a5cc4b0c818e13a14f53c9&oe=5CDB7115"
   },
   {
     title: "Lily Zhai",
-    description: "Programming 3"
+    description: "Programming 3",
+    picture:
+      "https://scontent-lga3-1.xx.fbcdn.net/v/t1.15752-9/51290748_318228555494566_4388106002570936320_n.png?_nc_cat=102&_nc_ht=scontent-lga3-1.xx&oh=7f5abde975c70d70f70d688b28ae0174&oe=5CEF9ECE"
   },
   {
-    title: "Pitom Schrute",
-    description: "Agriculture"
+    title: "Dwight Schrute",
+    description: "Agriculture",
+    picture:
+      "https://i.pinimg.com/originals/61/05/9b/61059bce4fe12ace08fb1609348b6138.jpg"
   },
   {
     title: "Jim Beasely",
-    description: "Business"
+    description: "Business",
+    picture:
+      "https://img.buzzfeed.com/buzzfeed-static/static/2015-10/6/8/campaign_images/webdr07/someone-calculated-how-much-money-jim-spent-prank-2-3018-1444134583-0_dblbig.jpg"
   },
   {
     title: "Michael Malone",
-    description: "Accounting"
+    description: "Accounting",
+    picture:
+      "https://www.bluleadz.com/hs-fs/hubfs/Blog_pics/PrisonMike.jpeg?width=598&name=PrisonMike.jpeg"
   }
 ];
 
@@ -86,9 +100,7 @@ class MentorList extends Component {
               <List.Item actions={[<DrawerForm />]}>
                 <Skeleton avatar title={false} loading={item.loading} active>
                   <List.Item.Meta
-                    avatar={
-                      <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                    }
+                    avatar={<Avatar src={item.picture} />}
                     title={item.title}
                     description={item.description}
                   />
